a9956ebf9f510fcf22aed43c69e3d9d93601f62e
Jos3s <karatedkgt17@gmail.com>
2021-04-20 12:06:34 -0300

    Requisitando a lista de episódios da "api"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
+import { useEffect } from "react";
+import { Header } from "../components/Header/Header";
+
+export default function Home(props) {
   return (
-    <h1>Hello World</h1>
+    <p>{JSON.stringify(props.episodes)}</p>
   )
 };
+
+export async function getStaticProps() {
+  const response=await fetch("http://localhost:3333/episodes")
+  const data = await response.json()
+  return {
+    props:{
+      episodes: data,
+    },
+    revalidate:60*60*8,
+  }
+}
