c4b7b3e1561d85078a69173f28cca6fb44c43ee8
Jos3s <karatedkgt17@gmail.com>
2021-04-20 10:03:15 -0300

    Adicionando componentes ao App e sua estilização de exibição

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
 import "../styles/global.scss";
 
+import { Header } from "../components/Header/Header";
+import { Player } from "../components/Player/Player";
+
+import styles from "../styles/app.module.scss"
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header/>
+        <Component {...pageProps} />
+      </main>
+      <Player/>
+    </div>
+  )
 };
