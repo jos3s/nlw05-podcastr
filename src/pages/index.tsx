b8da26f18c8f075de4199508c9e7ab665790d2d7
Jos3s <karatedkgt17@gmail.com>
2021-04-21 13:31:37 -0300

    Adicionando o axios como dependência

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,35 @@
+import { GetStaticProps } from "next";
+
 import { useEffect } from "react";
 import { Header } from "../components/Header/Header";
+import { api } from "../services/api";
 
-export default function Home(props) {
-  return (
-    <p>{JSON.stringify(props.episodes)}</p>
-  )
-};
-
-export async function getStaticProps() {
-  const response=await fetch("http://localhost:3333/episodes")
-  const data = await response.json()
-  return {
-    props:{
-      episodes: data,
-    },
-    revalidate:60*60*8,
-  }
+type Episode = {
+	id: string;
+	title: string;
+	members: string;
 }
+
+type HomeProps = {
+	episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
+	return <p>{JSON.stringify(props.episodes)}</p>;
+}
+
+export const getStaticProps:  GetStaticProps = async () => {
+	const { data } = await api.get("episodes", {
+		params: {
+			_limit: 12,
+			_sort: "published_at",
+			_order:"desc"
+		}
+	});
+	return {
+		props: {
+			episodes: data,
+		},
+		revalidate: 60 * 60 * 8,
+	};
+};;
